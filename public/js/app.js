93451e49cf43eea9a8d75cb3aa3ca6f651cfa6fa
parasjain <parasj96@gmail.com>
2014-09-24 20:43:08 -0400

    Initial move to partial layout

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,23 @@
-var app = angular.module("myapp",[]);
-    app.controller("hn",["$scope","$http",function($scope,$http){
-    	$http.get('/api').success(function(data){
-    		$scope.news = data;
-    	});
-    }]);
+var app = angular.module("myapp",['ui.router']);
+
+	app.config(function($stateProvider, $urlRouterProvider) {
+	    
+	    $urlRouterProvider.otherwise('/home');
+	    
+	    $stateProvider
+	        .state('home', {
+	            url: '/home',
+	            templateUrl: 'partials/index.html'
+	        })
+	        .state('feed', {
+	            url: '/feed',
+	            templateUrl: 'partials/feed.html'
+	        });
+	        
+	});
+
+    // app.controller("hn",["$scope","$http",function($scope,$http){
+    // 	$http.get('/api').success(function(data){
+    // 		$scope.news = data;
+    // 	});
+    // }]);
